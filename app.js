01e39174b6bbbd98bb8371e32358a3bf3266db28
zubair12147 <muhammadzubair121470@gmail.com>
2023-07-20 16:23:45 +0500

    Middle version

--- app.js
+++ app.js
@@ -1,72 +1,116 @@
-require('dotenv').config();
-const express = require('express');
-const ejs = require('ejs');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const session = require('express-session');
-const passport = require('passport');
-const passportLocalMongoose = require('passport-local-mongoose');
-
+require("dotenv").config();
+const express = require("express");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
-app.use(express.static('public'));
-app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(session({
-  secret: "",
-  resave: false,
-  saveUninitialized:false
-}));
+app.use(
+  session({
+    secret: "This is a secret string",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-mongoose.connect('mongodb://localhost:27017/userDB');
+mongoose.connect("mongodb://localhost:27017/userDB");
 // mongoose.set('useCreateIndex',true);
 
 const userSchema = new mongoose.Schema({
-  email: {
+  username: {
     type: String,
     required: true,
-    index: {unique: true},
+    index: { unique: true },
     lowercase: true,
   },
   password: {
-    type: String,
-    required: true,
+    type: String
   },
 });
 
 userSchema.plugin(passportLocalMongoose);
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.get('/', (req, res) => {
-  res.render('home');
+app.get("/", (req, res) => {
+  res.render("home");
 });
 
-app.get('/login', (req, res) => {
-  res.render('login');
+app.get("/login", (req, res) => {});
+
+app.get("/secrets", (req, res) => {
+  console.log("Entered into secrets post");
+  if(req.isAuthenticated()){
+    res.render('secrets');
+  }else{
+    res.render('login');
+  }
 });
 
-app.get('/register', (req, res) => {
-  res.render('register');
+app.get("/register", (req, res) => {
+  res.render("register");
 });
-
-app.post('/register', (req, res) => {
-
-});
-
-app.post('/login', (req, res) => {
   
+app.post("/register", (req, res) => {
+  User.register(
+    { username: req.body.username },
+    req.body.password,
+    (err, user) => {
+      if (err) {
+        console.log("registration error");
+        console.log(err);
+        res.redirect("/register");
+      } else {
+        passport.authenticate("local")(req, res, () => {
+          console.log("authentication error");
+          res.redirect("/secrets");
+        });
+      }
+    }
+  );
 });
+
+app.post("/login", (req, res) => {
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
+
+  req.login(user, (err)=>{
+    if(err){
+      console.log(err);
+    }else{
+      passport.authenticate('local')(req,res,()=>{
+        res.redirect('/secrets');
+      })
+    }
+  })
+});
+
+
+app.get('/logout', function(req, res) {
+  req.logout(function(err) {
+    if (err) { return next(err); }
+    res.redirect('/');
+  });
+});
+
 
 app.listen(3000, () => {
-  console.log('server started on port: 3000');
+  console.log("server started on port: 3000");
 });
